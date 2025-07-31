2f386f7dc733ddd8270f981e2444693b3517db45
Kevin Xiao <aquarius.c.c.a@gmail.com>
2025-07-31 17:28:15 +0800

    chore: 使用 Element Plus 组件库

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,48 @@
-<script setup>
-</script>
-
 <template>
-  <!-- 這會對應到 <symbol id="icon-home"> -->
-  <SvgIcon iconClass="druid" className="nav-icon" color="#409EFF" />
+  <div class="mb-4">
+    <el-button>Default</el-button>
+    <el-button type="primary">Primary</el-button>
+    <el-button type="success">Success</el-button>
+    <el-button type="info">Info</el-button>
+    <el-button type="warning">Warning</el-button>
+    <el-button type="danger">Danger</el-button>
+  </div>
 
-  <!-- 加上不同 className 以套用不同樣式 -->
-  <SvgIcon iconClass="bug" className="sub-el-icon" />
+  <div class="mb-4">
+    <el-button plain>Plain</el-button>
+    <el-button type="primary" plain>Primary</el-button>
+    <el-button type="success" plain>Success</el-button>
+    <el-button type="info" plain>Info</el-button>
+    <el-button type="warning" plain>Warning</el-button>
+    <el-button type="danger" plain>Danger</el-button>
+  </div>
+
+  <div class="mb-4">
+    <el-button round>Round</el-button>
+    <el-button type="primary" round>Primary</el-button>
+    <el-button type="success" round>Success</el-button>
+    <el-button type="info" round>Info</el-button>
+    <el-button type="warning" round>Warning</el-button>
+    <el-button type="danger" round>Danger</el-button>
+  </div>
+
+  <div>
+    <el-button :icon="Search" circle />
+    <el-button type="primary" :icon="Edit" circle />
+    <el-button type="success" :icon="Check" circle />
+    <el-button type="info" :icon="Message" circle />
+    <el-button type="warning" :icon="Star" circle />
+    <el-button type="danger" :icon="Delete" circle />
+  </div>
 </template>
 
-
-<style scoped></style>
+<script lang="ts" setup>
+import {
+  Check,
+  Delete,
+  Edit,
+  Message,
+  Search,
+  Star,
+} from '@element-plus/icons-vue'
+</script>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
+import Cookies from 'js-cookie'
 import App from './App.vue'
 import router from './router'
 
@@ -13,10 +13,25 @@ import 'virtual:svg-icons-register'
 // global css
 import '@/assets/styles/index.scss' 
 
+// 使用 Element Plus 组件库
+import ElementPlus from "element-plus";
+import "element-plus/dist/index.css";
+import zhTw from "element-plus/es/locale/lang/zh-tw";
+import 'element-plus/theme-chalk/dark/css-vars.css'
+
+
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(GlobalComponents)
 app.use(router)
+
+// 使用element-plus 并且设置全局的大小
+app.use(ElementPlus, {
+  locale: zhTw,
+  // 支持 large、default、small
+  size: Cookies.get('size') || 'default'
+})
+
 
 app.mount('#app')
